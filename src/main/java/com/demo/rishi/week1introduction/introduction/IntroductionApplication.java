f8feb8afa4ad0186fed1ba4d28b97e85c80fd2fd
Rishi Singh <rs3949427@gmail.com>
2024-07-13 13:02:35 +0530

    setting up the springboot project

---
+++ src/main/java/com/demo/rishi/week1introduction/introduction/IntroductionApplication.java
@@ -0,0 +1,12 @@
+package com.demo.rishi.week1introduction.introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionApplication.class, args);
+	}
+
+}
